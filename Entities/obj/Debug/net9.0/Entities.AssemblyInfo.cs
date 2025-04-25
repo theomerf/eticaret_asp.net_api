a850a1a3af0416f075e5b06efe49e60d1e122c1a
theomerf <omerfarukyalcin09@hotmail.com>
2025-04-26 02:49:47 +0300

    1

--- Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net9.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219231784738bd2411b92d712e2337fbe3f9c335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61fd58c27f44276cf0b321eb02ba7875576853b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
